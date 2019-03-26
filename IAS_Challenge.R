f6615cfeaa69406b31406a8f62c56a9c68129c59
Shivank Garg <shivankgargdce@live.com>
2019-03-25 23:14:33 -0500

    Update IAS_Challenge.R

--- IAS_Challenge.R
+++ IAS_Challenge.R
@@ -1,10 +1,10 @@
 ##  Install packages
-list.of.packages <- c("shiny","dplyr","DT","readr","readxl","tidyr","xlsx","wordcloud")
+list.of.packages <- c("shiny","dplyr","DT","readr","readxl","tidyr","xlsx","wordcloud","stringr","formattable")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
 # 
 
-library(shiny)
+
 library(dplyr)
 library(DT)
 library(readr)
@@ -13,6 +13,8 @@ library(tidyr)
 library(xlsx)
 library(ggplot2)
 library(wordcloud) #wordcloud
+library(stringr)
+library(formattable)
 
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 mov_budget.df <- read_excel("./IAC_4.0/IAC_4.0/IAC4-dataDescription.xlsx", 
@@ -50,4 +52,100 @@ mov_budget.df %>% select(title,Budget) %>%
         scale_y_continuous(labels=scales::comma)+
         labs(x="",y="Total Budget in $",title="Most Expensive Movies -Top 10")
 
+#Extract no of awards won per award category by each movie
+mov_budget.df$awards<- tolower(mov_budget.df$awards)
+mov_budget.df$wins <- str_extract(string = mov_budget.df$awards, "\\d+\\b(?=\\swin)")
+mov_budget.df$nomination <- str_extract(string = mov_budget.df$awards, "\\d+\\b(?=\\snomination)")
+mov_budget.df$oscars <- str_extract(string = mov_budget.df$awards, "\\d+\\b(?=\\soscar)")
+mov_budget.df$golden_globe <- str_extract(string = mov_budget.df$awards, "\\d+\\b(?=\\sgolden globe)")
 
+#Create dummy variable for sequel
+mov_budget.df$sequel <- ifelse(grepl("sequel",mov_budget.df$keywords),yes=1,no=0)
+
+credit %>% 
+  group_by(name) %>% 
+    tally() %>% 
+      arrange(desc(n)) %>% 
+        head(10) %>% 
+          ggplot(aes(factor(name,levels=name),n,fill=name))+
+          geom_bar(stat="identity")+labs(x="Artist",y="Count",title="Top 10 artist with most movies")+
+            theme_few()+
+              theme(axis.text.x=element_text(angle=90),
+                    plot.title=element_text(hjust=0.5,color="red"),
+                    legend.position="none")
+
+mov_budget <- mov_budget.df[complete.cases(mov_budget.df), ]
+dim(mov_budget)
+
+
+a<- as.data.frame(str_split_fixed(mov_budget.df$actors, ", ", Inf))
+b<- as.data.frame(mov_budget.df %>% separate(actors, actor1, sep = ", ", remove = TRUE, convert = FALSE))
+
+d<- mov_budget.df %>%
+  separate(director, into = c("director1", "director2"), sep=", ")
+
+sapply(d,function(x){sum(is.na(x))})
+
+
+#Directors average rating. Only for directors who have >5 movies in our database
+d %>%
+  group_by(director1) %>%
+    summarise(Tot.films = n(), avg_rating = mean(movie_rating__1)) %>%
+        arrange(desc(avg_rating)) %>%
+          top_n(30, avg_rating) %>%
+            formattable(list(avg_rating = color_bar("orange")), align = 'l')
+
+d %>%
+  group_by(director2) %>%
+    summarise(Tot.films = n(), avg_rating = mean(movie_rating__1)) %>%
+      filter(Tot.films>5) %>%
+        arrange(desc(avg_rating)) %>%
+          top_n(30, avg_rating) %>%
+            formattable(list(avg_rating = color_bar("orange")), align = 'l')
+
+#Actors average rating. Only for actors that have >5 movies in our database
+mov_budget.df %>%
+  str_split_fixed(mov_budget.df$actors, ", ") %>%
+    group_by(str_split_fixed(mov_budget.df$actors, ", ", Inf)) %>%
+      summarise(Tot.films = n(), avg_rating = mean(movie_rating__1)) %>%
+        filter(Tot.films>5) %>%
+          arrange(desc(avg_rating)) %>%
+            top_n(30, avg_rating) %>%
+              formattable(list(avg_rating = color_bar("orange")), align = 'l')
+
+
+
+a<- mov_budget.df %>%
+  separate(actors, into = c("actor1", "actor2","actor3","actor4"), sep=", ")
+
+a %>%
+  group_by(actor1) %>%
+  summarise(Tot.films = n(), avg_rating = mean(movie_rating__1)) %>%
+  filter(Tot.films>5) %>%
+  arrange(desc(avg_rating)) %>%
+  top_n(10, avg_rating) %>%
+  formattable(list(avg_rating = color_bar("orange")), align = 'l')
+
+a %>%
+  group_by(actor2) %>%
+  summarise(Tot.films = n(), avg_rating = mean(movie_rating__1)) %>%
+  filter(Tot.films>5) %>%
+  arrange(desc(avg_rating)) %>%
+  top_n(10, avg_rating) %>%
+  formattable(list(avg_rating = color_bar("orange")), align = 'l')
+
+a %>%
+  group_by(actor3) %>%
+  summarise(Tot.films = n(), avg_rating = mean(movie_rating__1)) %>%
+  filter(Tot.films>5) %>%
+  arrange(desc(avg_rating)) %>%
+  top_n(10, avg_rating) %>%
+  formattable(list(avg_rating = color_bar("orange")), align = 'l')
+
+a %>%
+  group_by(actor4) %>%
+  summarise(Tot.films = n(), avg_rating = mean(movie_rating__1)) %>%
+  filter(Tot.films>5) %>%
+  arrange(desc(avg_rating)) %>%
+  top_n(10, avg_rating) %>%
+  formattable(list(avg_rating = color_bar("orange")), align = 'l')
